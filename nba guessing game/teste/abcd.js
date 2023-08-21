e673392bf0d0321e6ab8de37ce3efaa2851d5829
gama-gabriel <bielmgama@gmail.com>
2023-08-21 16:48:47 -0300

    Delete abcd.js

--- nba guessing game/teste/abcd.js
+++
@@ -1,7 +0,0 @@
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Example usage:
-const randomNumber = getRandomInt(1, 468); // Generates a random number between 1 and 10 (inclusive)
-console.log(randomNumber);
