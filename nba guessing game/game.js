8f08ff5fc453226983b63a9835e158b0bc707e34
gama-gabriel <bielmgama@gmail.com>
2023-08-20 23:28:21 -0300

    resultados

--- nba guessing game/game.js
+++ nba guessing game/game.js
@@ -1,9 +1,9 @@
 let nome = window.document.getElementById('Nome')
-let posicao = window.document.getElementById('Pos')
+let posicao = window.document.getElementById('pos')
 let pontos = window.document.getElementById('points')
 let idade = document.getElementById('age')
 let rebotes = document.getElementById('rebounds')
-let time = document.getElementById('team')
+let time = document.getElementById('Team')
 let assistencias = document.getElementById('assists')
 let fg = document.getElementById('fg')
 let tres_p = document.getElementById('3p')
@@ -14,6 +14,30 @@ let blk = document.getElementById('blk')
 const barraPesquisa = document.getElementById('busca')
 const resultados = document.getElementById('resultados')
 let itensBuscados = 0
+let tentativas = 1
+
+function criar_linha(nome_jogador, time_jogador, idade_jogador, posicao_jogador, ppg_jogador, ast_jogador, reb_jogador, fg_jogador, tres_p_jogador, ft_jogador, stl_jogador, blk_jogador)
+{
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+    <td id="Nome" class="nome">${nome_jogador}</td>
+    <td id="Team" class="stats">${time_jogador}</td>
+    <td id="age" class="stats">${idade_jogador}</td>
+    <td id="pos" class="stats">${posicao_jogador}</td>
+    <td id="points" class="stats">${ppg_jogador}</td>
+    <td id="assists" class="stats">${ast_jogador}</td> 
+    <td id="rebounds" class="stats">${reb_jogador}</td> 
+    <td id="fg" class="stats">${fg_jogador}</td> 
+    <td id="3p" class="stats">${tres_p_jogador}</td> 
+    <td id="ft" class="stats">${ft_jogador}</td> 
+    <td id="stl" class="stats">${stl_jogador}</td> 
+    <td id="blk" class="stats">${blk_jogador}</td> 
+    
+    `
+    tabela = document.getElementById('resp1')
+    linha = document.getElementById('linha')
+    tabela.insertBefore(newRow, linha.parentNode)
+}
 
 barraPesquisa.addEventListener('input', buscar)
 
@@ -37,7 +61,7 @@ async function buscar()
             const lista = document.createElement('li')
             lista.textContent = `${item.PName}`
             lista.setAttribute('id', item.id)
-            lista.addEventListener('click', () => selecionarJog(item.PName, item.Team, item.POS, item.PPG, item.APG))
+            lista.addEventListener('click', () => selecionarJog(item.id, item.PName, item.Team, item.Age, item.POS, item.PPG, item.APG, item.RPG, item["FG%"], item["3P%"], item["FT%"], item.SPG, item.BPG))
             resultados.appendChild(lista)
             itensBuscados++
         }
@@ -46,13 +70,13 @@ async function buscar()
  
 }
 
-function selecionarJog(nome_jogador, posicao_jogador, ppg_jogador, ast_jogador) 
+function selecionarJog(id_jogador, nome_jogador, time_jogador, idade_jogador, posicao_jogador, ppg_jogador, ast_jogador, reb_jogador, fg_jogador, tres_p_jogador, ft_jogador, stl_jogador, blk_jogador) 
 {
-    nome.innerText = nome_jogador
-    posicao.innerText = posicao_jogador
-    pontos.innerText = ppg_jogador
-    assistencias.innerText = ast_jogador
+
     resultados.innerHTML = ''
-    barraPesquisa.value = ''   
+    barraPesquisa.value = '' 
+    criar_linha(nome_jogador, time_jogador, idade_jogador, posicao_jogador, ppg_jogador, ast_jogador, reb_jogador, fg_jogador, tres_p_jogador, ft_jogador, stl_jogador, blk_jogador)
+    tentativas++
+      
     
 }
